eaa61561b41f54c15ab8a8641724d27748c203c6
Ertuğrul Soylu <75154294+ErtugrulSoylu@users.noreply.github.com>
2022-07-18 09:06:54 +0300

    Add files via upload

--- ucus_komutlari.py
+++ ucus_komutlari.py
@@ -19,6 +19,40 @@ class aero:
         self.myDrone.havadaTurlama()
         print('success!')
 
+    def video(self):
+        self.myDrone.arm_and_takeoff(5)
+        location = self.vehicle.location.local_frame
+        shift = 3
+        
+        self.myDrone.go_to(location.north + shift, location.east + shift, location.down, 0)
+        while (self.vehicle.location.local_frame.north < location.north + shift - 0.3 \
+            or self.vehicle.location.local_frame.east < location.east + shift - 0.3):
+            time.sleep(1)
+        
+        self.myDrone.go_to(location.north + shift, location.east - shift, location.down, 0)
+        while (self.vehicle.location.local_frame.north < location.north + shift - 0.3 \
+            or self.vehicle.location.local_frame.east > location.east - shift + 0.3):
+            time.sleep(1)
+
+        self.myDrone.go_to(location.north - shift, location.east - shift, location.down, 0)
+        while (self.vehicle.location.local_frame.north > location.north - shift + 0.3 \
+            or self.vehicle.location.local_frame.east > location.east - shift + 0.3):
+            time.sleep(1)
+        
+        self.myDrone.go_to(location.north - shift, location.east + shift, -1.5, 0)
+        while (self.vehicle.location.local_frame.north > location.north - shift + 0.3 \
+            or self.vehicle.location.local_frame.east < location.east + shift - 0.3 \
+            or self.vehicle.location.local_frame.down < -1.5 - 0.1):
+            time.sleep(1)
+        
+        self.myDrone.go_to(self.vehicle.location.local_frame.north, self.vehicle.location.local_frame.east, -1, 0)
+        print("puskurt!!!!!!")
+        for i in range(5):
+            time.sleep(1)
+            print("pssss")
+        print("bitti")
+        self.myDrone.immadiateLanding()
+
     def otonom_kalkis_inis(self, yukseklik : int = 1, saniye : int = 3):
         yukseklik = 5
         saniye = 3
